d0437b56567fe2272bd76fd07bd0cf64e4151166
JaredTiu <jaredtiu1706@gmail.com>
2026-02-28 22:25:23 +0800

    added classes, and cors middleware

--- Backend/main.py
+++ Backend/main.py
@@ -7,8 +7,23 @@ from typing import List
 class Shopping(BaseModel):
     name: str
 
+class Fruits(BaseModel):
+    fruits: List[Shopping]
+
+class Meats(BaseModel):
+    meats: List[Shopping]
+
 app = FastAPI()
 
 origins = [
     "http://localhost:3000",
 ]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
